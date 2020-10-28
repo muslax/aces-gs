30e62ee63b7103456293882a5253eb100cdbc627
muslax <muslax@gmail.com>
2020-10-28 09:08:58 +0700

    Update set-modules.jsx

--- src/pages/[license]/projects/[id]/set-modules.jsx
+++ src/pages/[license]/projects/[id]/set-modules.jsx
@@ -136,11 +136,11 @@ export default function SetModules({ info, modules }) {
   // if(!license || !user || license.slug != user?.license) return NotFound
 
   return (
-    <FormLayout info={info}>
+    <div info={info}>
       <Form user={user} info={info} modules={modules} />
       {/* <pre className="pre">{JSON.stringify(info, null, 2)}</pre> */}
       {/* <pre className="pre">{JSON.stringify(modules, null, 2)}</pre> */}
-    </FormLayout>
+    </div>
   )
 }
 
